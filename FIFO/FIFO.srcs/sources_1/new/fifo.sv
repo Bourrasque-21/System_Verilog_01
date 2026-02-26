f199010e8b638149985b3566b8ea12577645c13e
Aspera <sciabole@icloud.com>
2026-02-26 23:45:27 +0900

    add instance

--- FIFO/FIFO.srcs/sources_1/new/fifo.sv
+++ FIFO/FIFO.srcs/sources_1/new/fifo.sv
@@ -1,5 +1,40 @@
 `timescale 1ns / 1ps
 
+module fifo (
+    input clk,
+    input rst,
+    input push,
+    input pop,
+    input [7:0] wdata,
+    output [7:0] rdata,
+    output full,
+    output empty
+);
+
+    logic [3:0] w_rptr, w_wptr;
+
+
+    sram U_REGISTER (
+        .clk  (clk),
+        .addr (w_wptr),
+        .wdata(w_rptr),
+        .we   (push & (~full)),
+        .rdata(rdata)
+    );
+
+    control_unit U_CONTROL (
+        .clk  (clk),
+        .rst  (rst),
+        .push (push),
+        .pop  (pop),
+        .full (full),
+        .empty(empty),
+        .wptr (w_wptr),
+        .rptr (w_rptr)
+    );
+
+
+endmodule
 
 
 module control_unit (
@@ -25,8 +60,8 @@ module control_unit (
         if (rst) begin
             full_r  <= 0;
             empty_r <= 1;
-            wptr_r  <= 0;
-            rptr_r  <= 0;
+            wptr_r  <= 4'd0;
+            rptr_r  <= 4'd0;
         end else begin
             full_r  <= full_n;
             empty_r <= empty_n;
@@ -46,33 +81,33 @@ module control_unit (
         })
             2'b10: begin  // Push
                 if (!full_r) begin
-                    wptr_n  = wptr_r + 1;
+                    wptr_n  = wptr_r + 4'd1;
                     empty_n = 0;
-                    if ((wptr_r + 1) == rptr_r) begin
+                    if ((wptr_r + 4'd1) == rptr_r) begin
                         full_n = 1;
                     end
                 end
             end
             2'b01: begin  // Pop
                 if (!empty_r) begin
-                    rptr_n = rptr_r + 1;
+                    rptr_n = rptr_r + 4'd1;
                     full_n = 0;
-                    if ((rptr_r + 1) == wptr_r) begin
+                    if ((rptr_r + 4'd1) == wptr_r) begin
                         empty_n = 1;
                     end
                 end
             end
             2'b11: begin  // Push & Pop
                 if (full_r) begin
-                    rptr_n = rptr_r + 1;
-                    wptr_n = wptr_r + 1;
+                    rptr_n = rptr_r + 4'd1;
+                    wptr_n = wptr_r + 4'd1;
                     full_n = 1;
                 end else if (empty_r) begin
-                    wptr_n  = wptr_r + 1;
+                    wptr_n  = wptr_r + 4'd1;
                     empty_n = 0;
                 end else begin
-                    rptr_n = rptr_r + 1;
-                    wptr_n = wptr_r + 1;
+                    rptr_n = rptr_r + 4'd1;
+                    wptr_n = wptr_r + 4'd1;
                 end
             end
         endcase
