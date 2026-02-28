6ea9999ed1191014bde64bc486bc868e66c7e211
Aspera <sciabole@icloud.com>
2026-02-28 21:46:50 +0900

    fifo

--- FIFO/FIFO.srcs/sources_1/new/fifo.sv
+++ FIFO/FIFO.srcs/sources_1/new/fifo.sv
@@ -1,33 +1,33 @@
 `timescale 1ns / 1ps
 
-module fifo (
-    input        clk,
-    input        rst,
-    input        push,
-    input        pop,
-    input  [7:0] wdata,
-    output [7:0] rdata,
-    output       full,
-    output       empty
+module fifo_sv (
+    input  logic       clk,
+    input  logic       rst,
+    input  logic       we,
+    input  logic       re,
+    input  logic [7:0] wdata,
+    output logic [7:0] rdata,
+    output logic       full,
+    output logic       empty
 );
 
     logic [3:0] w_rptr, w_wptr;
 
 
-    register_file U_REGISTER (
+    register_file_sv U_REGISTER (
         .clk   (clk),
         .r_addr(w_rptr),
         .w_addr(w_wptr),
         .wdata (wdata),
-        .we    (push & (~full | pop)),
+        .we    (we & (~full)),
         .rdata (rdata)
     );
 
-    control_unit U_CONTROL (
+    control_unit_sv U_CONTROL (
         .clk  (clk),
         .rst  (rst),
-        .push (push),
-        .pop  (pop),
+        .push (we),
+        .pop  (re),
         .full (full),
         .empty(empty),
         .wptr (w_wptr),
@@ -38,15 +38,15 @@ module fifo (
 endmodule
 
 
-module control_unit (
-    input        clk,
-    input        rst,
-    input        push,
-    input        pop,
-    output       full,
-    output       empty,
-    output [3:0] wptr,
-    output [3:0] rptr
+module control_unit_sv (
+    input  logic       clk,
+    input  logic       rst,
+    input  logic       push,
+    input  logic       pop,
+    output logic       full,
+    output logic       empty,
+    output logic [3:0] wptr,
+    output logic [3:0] rptr
 );
 
     logic [3:0] rptr_r, rptr_n, wptr_r, wptr_n;
@@ -84,7 +84,7 @@ module control_unit (
                 if (!full_r) begin
                     wptr_n  = wptr_r + 4'd1;
                     empty_n = 0;
-                    if ((wptr_r + 4'd1) == rptr_r) begin
+                    if (wptr_n == rptr_r) begin
                         full_n = 1;
                     end
                 end
@@ -93,7 +93,7 @@ module control_unit (
                 if (!empty_r) begin
                     rptr_n = rptr_r + 4'd1;
                     full_n = 0;
-                    if ((rptr_r + 4'd1) == wptr_r) begin
+                    if (rptr_n == wptr_r) begin
                         empty_n = 1;
                     end
                 end
@@ -101,8 +101,7 @@ module control_unit (
             2'b11: begin  // Push & Pop
                 if (full_r) begin
                     rptr_n = rptr_r + 4'd1;
-                    wptr_n = wptr_r + 4'd1;
-                    full_n = 1;
+                    full_n = 0;
                 end else if (empty_r) begin
                     wptr_n  = wptr_r + 4'd1;
                     empty_n = 0;
@@ -116,8 +115,8 @@ module control_unit (
 endmodule
 
 
-module register_file (
-    input              clk,
+module register_file_sv (
+    input  logic       clk,
     input  logic [3:0] w_addr,
     input  logic [3:0] r_addr,
     input  logic [7:0] wdata,
